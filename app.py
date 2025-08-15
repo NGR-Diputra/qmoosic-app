38e69a9afd4c6989310c58ae3ab90f776b3013d2
NGRH-Diputra <120901630+NGR-Diputra@users.noreply.github.com>
2025-08-15 22:17:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -67,4 +67,7 @@ def show_result():
     )
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    from waitress import serve
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    serve(app, host="0.0.0.0", port=port
