ecced6e5dda7ceae6874300bb571445ff75e6c77
NGRH-Diputra <120901630+NGR-Diputra@users.noreply.github.com>
2025-08-15 22:53:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -65,9 +65,3 @@ def show_result():
         audio_filename=filename,
         quadrant_image=image_filename
     )
-
-if __name__ == '__main__':
-    from waitress import serve
-    import os
-    port = int(os.environ.get("PORT", 8080))
-    serve(app, host="0.0.0.0", port=port
